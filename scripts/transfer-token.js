3bad96769469e8513e1c61fb0bd3923cd7949b98
patelvishal1401 <patelvishal1401@gmail.com>
2021-08-31 10:52:12 +0530

    first commit

--- scripts/transfer-token.js
+++ scripts/transfer-token.js
@@ -4,7 +4,7 @@ module.exports = async function (done) {
 
     const mdaToken = await MDAToken.deployed()
     
-    await mdaToken.transfer('0x45AdB2B996aF4146357E24Df6D52fdE1ba3e07AE', '100000000000000000000')
+    await mdaToken.transfer('0x', '100000000000000000000')  // trsanfer 100 MDA, Pass your receiver account in first parameter
     
     done();
 };
